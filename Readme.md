69a7c0785bc2a46d07641653f31d60329252061a
PilaPina <solald@gmail.com>
2023-09-26 21:48:39 +0000

    whole lot of changes and comments

--- Readme.md
+++ Readme.md
@@ -10,3 +10,23 @@ Submit:
 Minimum requirements:
 1. A website that uses at least 2 different APIs
 2. Use as many features of the API as you can
+
+WHAT IS A WEB API?
+
+- API stands for Application Programming Interface.
+
+- A Web API is an application programming interface for the Web.
+
+- A Browser API can extend the functionality of a web browser.
+
+- A Server API can extend the functionality of a web server.
+
+- a Web API is like a menu for computer programs. It helps different programs talk to each other and share information. You can use it to get data or perform actions without having to do all the work yourself. It's like asking a waiter for your favorite dish instead of cooking it from scratch.
+
+- They are a list of rules and info that a website or an app provides to others. These rules explain what data and services are available and how to ask for them. 
+Web APIs enable different software systems, often running on different platforms and written in different programming languages, to communicate and exchange data. This promotes interoperability and allows developers to leverage existing services and data in their own applications.
+
+- Web APIs are used in various scenarios, including fetching data from social media platforms (e.g., Twitter API, Facebook Graph API), accessing weather data (e.g., OpenWeatherMap API), integrating with payment gateways (e.g., PayPal API), and many more.
+
+- Third party APIs are not built into your browser.  To use these APIs, you will have to download the code from the Web.
+Examples:  YouTube API - Allows you to display videos on a web site.   Twitter API - Allows you to display Tweets on a web site.  Facebook API - Allows you to display Facebook info on a web site.
---
+++ main.js
@@ -0,0 +1,46 @@
+"use strict";
+/* creating constants for the slidecontainer and buttons
+The as HTMLElement syntax (it's a type assertion)
+tells Typescript to treat the result of the document.querySelector('.carousel-slide')
+as a HTML element object. (we know for sure this will be an object and not null)
+This is necessary because the querySelector method has a return type of ' Element | null '
+*/
+// These get elements from the DOM
+const slideContainer = document.querySelector('.carousel-slide');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+// Set the inital slide index. 
+let currentIndex = 0; // 0 is the first image in the carousel
+// console.log(currentIndex);  //checking if it works
+const slideWidth = 430;
+//Then we need a function so the carousel works as a carousel . See comment below. 
+// We do that by showing one image at a time and we add transitions in the CSS
+function showSlide(index) {
+    const translateX = -index * slideWidth;
+    slideContainer.style.transform = `translateX(${translateX}px)`;
+}
+/*  This comment is for the function above.
+    Calculate the translateX value based on the index.
+    Index tells us how much to move the slide container horizontally
+    The transform property moves the images to the left or right
+    The value '* 400' needs to fit the image width
+*/
+//then we need an eventListener for buttons. See comment below. 
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slideContainer.children.length) % slideContainer.children.length;
+    showSlide(currentIndex);
+});
+/*  Comment for code above:
+    currentIndex: This is a variable that keeps track of the currently displayed image's index.
+    '-': This is the subtraction operator. It subtracts 1 from currentIndex and moves the previous image.
+    '+': This is the addition operator. It adds slideContainer.children.length to the result of the subtraction. This is to wrap around the last image.
+    '%': This is the modulo operator. It calculates the remainder when dividing the result of the addition by slideContainer.children.length.
+    The modulo ensures that the resulting index remains within the valid range of indices for the image in the carousle.
+    This wraps the first image to the last image.
+    */
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slideContainer.children.length;
+    showSlide(currentIndex);
+});
+// the the results
+showSlide(currentIndex);
